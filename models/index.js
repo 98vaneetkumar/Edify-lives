ca27c7115c85ed280df1caa3d418307e9a9d05a1
98vaneetkumar <vaneet3157@gmail.com>
2025-02-09 13:39:56 +0530

    refactor model imports in index.js to use updated file names

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const sequelize = require("../dbConnection").sequelize;
 module.exports = {
   userModel: require("./userModel")(Sequelize, sequelize, Sequelize.DataTypes),
   cmsModel: require("./cmsModel")(Sequelize, sequelize, Sequelize.DataTypes),
-  notificationModel:require("./notificationModel")(Sequelize, sequelize, Sequelize.DataTypes),
-  contactUsModel:require("./contactUsModel")(Sequelize, sequelize, Sequelize.DataTypes),
+  notificationModel:require("./notificationModels")(Sequelize, sequelize, Sequelize.DataTypes),
+  contactUsModel:require("./contactUs")(Sequelize, sequelize, Sequelize.DataTypes),
 };
  
