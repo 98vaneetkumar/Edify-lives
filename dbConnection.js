1e4f1c7e9bccf7e58fbd0cb2ff6094c8283b80ed
98vaneetkumar <vaneet3157@gmail.com>
2025-02-09 13:34:20 +0530

    update db connection sync behavior to allow alterations

--- dbConnection.js
+++ dbConnection.js
@@ -13,12 +13,12 @@ const connectdb = async () => {
   await sequelize
     .authenticate()
     .then(async () => {
-      await sequelize.sync({ alter: false });
+      await sequelize.sync({ alter: true });
       console.log("db is connected and sync also");
-    })
+    }) 
     .catch((err) => {
       console.log("error while connecting to the db", err);
-      throw err;
+      throw err; 
     });
 };
 
