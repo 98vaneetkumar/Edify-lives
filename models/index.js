8bb8686aa80e6de6d4f74735623cb0894203f2f1
98vaneetkumar <vaneet3157@gmail.com>
2025-02-09 13:37:36 +0530

    add notification and contactUs models to the index

--- models/index.js
+++ models/index.js
@@ -4,4 +4,7 @@ const sequelize = require("../dbConnection").sequelize;
 module.exports = {
   userModel: require("./userModel")(Sequelize, sequelize, Sequelize.DataTypes),
   cmsModel: require("./cmsModel")(Sequelize, sequelize, Sequelize.DataTypes),
+  notificationModel:require("./notificationModel")(Sequelize, sequelize, Sequelize.DataTypes),
+  contactUsModel:require("./contactUsModel")(Sequelize, sequelize, Sequelize.DataTypes),
 };
+ 
