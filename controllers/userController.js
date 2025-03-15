cd960dd4fe27ccfc20d82b19e6ec7882074f82e9
98vaneetkumar <vaneet3157@gmail.com>
2025-03-15 23:30:40 +0530

    fix: simplify groupModel query by removing redundant case statement for improved performance

--- controllers/userController.js
+++ controllers/userController.js
@@ -1234,14 +1234,7 @@ module.exports = {
       }
       let response=await Models.groupModel.findAndCountAll({
         attributes: {
-          include: [
-            [Sequelize.literal(`
-              (CASE 
-                WHEN (SELECT count(id) FROM group where userId = '${req.user.id}') > 0 
-                THEN 1 
-                ELSE 0 
-              END)
-              `),"iCreated"],  
+          include: [  
             [Sequelize.literal("(SELECT count(id) FROM commentGroup where groupId=group.id )"), "commentsCount"],
             [Sequelize.literal("(SELECT count(id) FROM likeGroup where groupId=group.id )"), "likesCount"],
             [Sequelize.literal(`
